7e28009e5be1fec3e95dbdc3896dc40db4649932
Daniel-Sinkin <danielsinkin97@gmail.com>
2025-07-11 00:18:38 +0200

    added .vscode setting

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,23 @@
+#include <iostream>
 #include <print>
+
+#include <Eigen/Dense>
 
 #include "common.hpp"
 
 int main() {
+    using namespace Eigen;
+    MatrixXd M(2, 2);
+    M << 3, -1,
+        2.5, M(1, 0) + M(0, 1);
+
+    VectorXd v(3);
+    v << 1, 2, 3;
+
+    VectorXd result = M * v.head(2);
+
+    std::cout << "M:\n"
+              << M << "\nresult:\n"
+              << result << '\n';
     return 0;
 }
